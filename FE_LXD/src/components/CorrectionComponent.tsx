b56043e2ad1771d3d297611aa86baec9ea83859f
cchaeyoon <codbs0722@gmail.com>
2025-07-10 23:15:10 +0900

    feature:ProfileComponent 제작

--- FE_LXD/src/components/CorrectionComponent.tsx
+++ FE_LXD/src/components/CorrectionComponent.tsx
@@ -1,7 +1,9 @@
+import ProfileComonent from "./ProfileComonent"
+
 const CorrectionComponent = () => {
   return (
-    <div>
-      하이
+    <div className="w-200 h-81 bg-white rounded-[10px] border border-gray-300">
+      <ProfileComonent />
     </div>
   )
 }
