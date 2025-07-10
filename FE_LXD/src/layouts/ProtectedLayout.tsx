9c1047c10b83ab1c5c435c78f4cc346ed42486f2
seokhoon <excava.hoon76@gmail.com>
2025-07-10 21:16:04 +0900

    fix : ProtectLayout style

--- FE_LXD/src/layouts/ProtectedLayout.tsx
+++ FE_LXD/src/layouts/ProtectedLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const ProtectedLayout = () => {
   return (
     <div className="relative min-h-screen">
-      <div className="">
+      <div>
         <SideBar />
       </div>
       <div className="ml-[240px] p-6">
