755d359a7da5e668d0fbd5601041d5fd433fcd1b
seokhoon <excava.hoon76@gmail.com>
2025-07-12 15:37:40 +0900

    merging feature/writing & develop

--- FE_LXD/src/components/SideBar.tsx
+++ FE_LXD/src/components/SideBar.tsx
@@ -13,14 +13,18 @@ const Sidebar = () => {
               "w-[204px] h-[55px] rounded-[5px] px-[10px] py-[15px] hover:scale-105 transition-transform duration-300",
               isActive
                 ? "bg-[#F1F5FD] text-blue-600 font-semibold"
-                : "text-gray-600 font-semibold"
+                : "text-gray-600 font-semibold",
             ].join(" ")
           }
         >
           {({ isActive }) => (
             <>
-              <img 
-                src={isActive ? "/images/FeedOnIcon.svg" : "/images/FeedOffIcon.svg"}
+              <img
+                src={
+                  isActive
+                    ? "/images/FeedOnIcon.svg"
+                    : "/images/FeedOffIcon.svg"
+                }
                 alt="피드 아이콘"
               />
               피드
@@ -35,14 +39,18 @@ const Sidebar = () => {
               "w-[204px] h-[55px] rounded-[5px] px-[10px] py-[15px] hover:scale-105 transition-transform duration-300",
               isActive
                 ? "bg-[#F1F5FD] text-blue-600 font-semibold"
-                : "text-gray-600 font-semibold"
+                : "text-gray-600 font-semibold",
             ].join(" ")
           }
         >
           {({ isActive }) => (
             <>
-              <img 
-                src={isActive ? "/images/DiaryOnIcon.svg" : "/images/DiaryOffIcon.svg"}
+              <img
+                src={
+                  isActive
+                    ? "/images/DiaryOnIcon.svg"
+                    : "/images/DiaryOffIcon.svg"
+                }
                 alt="다이어리 아이콘"
               />
               나의 다이어리
@@ -57,14 +65,18 @@ const Sidebar = () => {
               "w-[204px] h-[55px] rounded-[5px] px-[10px] py-[15px] hover:scale-105 transition-transform duration-300",
               isActive
                 ? "bg-[#F1F5FD] text-blue-600 font-semibold"
-                : "text-gray-600 font-semibold"
+                : "text-gray-600 font-semibold",
             ].join(" ")
           }
         >
           {({ isActive }) => (
             <>
-              <img 
-                src={isActive ? "/images/CorrectOnIcon.svg" : "/images/CorrectOffIcon.svg"}
+              <img
+                src={
+                  isActive
+                    ? "/images/CorrectOnIcon.svg"
+                    : "/images/CorrectOffIcon.svg"
+                }
                 alt="교정 아이콘"
               />
               나의 교정
@@ -79,14 +91,18 @@ const Sidebar = () => {
               "w-[204px] h-[55px] rounded-[5px] px-[10px] py-[15px] hover:scale-105 transition-transform duration-300",
               isActive
                 ? "bg-[#F1F5FD] text-blue-600 font-semibold"
-                : "text-gray-600 font-semibold"
+                : "text-gray-600 font-semibold",
             ].join(" ")
           }
         >
           {({ isActive }) => (
             <>
-              <img 
-                src={isActive ? "/images/FriendOnIcon.svg" : "/images/FriendOffIcon.svg"}
+              <img
+                src={
+                  isActive
+                    ? "/images/FriendOnIcon.svg"
+                    : "/images/FriendOffIcon.svg"
+                }
                 alt="친구"
               />
               친구
@@ -95,9 +111,7 @@ const Sidebar = () => {
         </NavLink>
       </nav>
 
-      <button
-        className="mt-auto flex items-center gap-[12px] w-[204px] h-[55px] rounded-[5px] text-subhead3 px-[10px] py-[15px] text-gray-600 font-semibold cursor-pointer hover:scale-105 transition-transform duration-300"
-      >
+      <button className="mt-auto flex items-center gap-[12px] w-[204px] h-[55px] rounded-[5px] text-subhead3 px-[10px] py-[15px] text-gray-600 font-semibold cursor-pointer hover:scale-105 transition-transform duration-300">
         <img
           src="/images/SettingIcon.svg"
           alt="설정 아이콘"
--- FE_LXD/src/pages/Diary/WritingPage.tsx
+++ FE_LXD/src/pages/Diary/WritingPage.tsx
@@ -1,31 +1,33 @@
+import ModeSelector from "../../components/ModeSelector";
 import PrevButton from "../../components/PrevButton";
 import TitleHeader from "../../components/TitleHeader";
 
 const WritingPage = () => {
   return (
     <div className="px-4 py-2 bg-gray-50">
-      <div className="flex items-center gap-x-6">
+      <div className="flex items-center gap-x-6 ">
         <PrevButton navigateURL="/diary" />
         <TitleHeader title="글쓰기" />
+        <button className="rounded-[8px] bg-[var(--Primary-500,#4170FE)] text-white hover:bg-[var(--Primary-600,#3259D9)] p-3 transition-all duration-300 cursor-pointer">
+          등록하기
+        </button>
       </div>
-      <div className=" flex flex-col items-start p-5 gap-[15px] self-stretch ">
+      <div className=" flex flex-col items-start p-5 gap-[15px] self-stretch w-full">
         {/*자유글 / 질문글 구분 영역*/}
-
         <div className="bg-white rounded-[12px] shadow-[0px_4px_10px_rgba(0,0,0,0.1)] mt-5 w-full p-5">
-          <div className="flex items-center gap-x-2 mb-2">
-            <button className="rounded-[5px] bg-[#DDE0E4] cursor-pointer">
-              질문글
-            </button>
-            <button className="rounded-[5px] bg-[#DDE0E4] cursor-pointer">
-              자유글
-            </button>
-          </div>
+          <ModeSelector title1="자유글" title2="질문글" />
           질문글 / 자유글 선택
         </div>
 
         {/*글쓰기 영역*/}
-        <div className="bg-white rounded-[12px] shadow-[0px_4px_10px_rgba(0,0,0,0.1)] mt-5 w-full p-5">
-          대충 내용 넣는 부분{" "}
+
+        <div className="flex items-center gap-x-2 mb-2">
+          <label className="text-[var(--Gray-700,#4B5563)]">태그</label>
+          <input
+            type="text"
+            className="border border-gray-300 rounded-md p-2 w-full"
+            placeholder="태그를 입력하세요."
+          />
         </div>
       </div>
     </div>
