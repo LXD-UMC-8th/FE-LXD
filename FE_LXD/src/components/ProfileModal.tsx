8b1b54a1fcf3cd76eccfd0b1da2c0dd99067eb10
cchaeyoon <codbs0722@gmail.com>
2025-07-17 02:11:51 +0900

    button refact

--- FE_LXD/src/components/ProfileModal.tsx
+++ FE_LXD/src/components/ProfileModal.tsx
@@ -11,18 +11,35 @@ const NavProfileModal = ({onClose}: ProfileModalProps) => {
   const navigate = useNavigate();
   const [showlogoutModal, setShowlogoutModal ] = useState(false);
 
-  const handleAction = (action: "edit" | "logout") => {
-    setActive(action);
-
-    if(action === "edit") {
-      navigate("/profileedit");
-    } else if(action === "logout") {
-      setShowlogoutModal(true);
-    }
-  };
+  const commonIcon = {
+    on: "/images/ExitOnIcon.svg",
+    off: "/images/ExitOffIcon.svg",
+  }
+  const actions: {
+    key: "edit" | "logout";
+    label: string;
+    onClick: () => void;
+  }[] =[
+    {
+      key: "edit",
+      label: "프로필 수정하기",
+      onClick: () => {
+        setActive("edit");
+        navigate("/profileedit");
+      }
+    },
+    {
+      key: "logout",
+      label: "로그아웃",
+      onClick: () => {
+        setActive("logout");
+        setShowlogoutModal(true);
+      },
+    },
+  ];
 
   return (
-    <div className="w-50 h-49 border-1 border-gray-300 bg-white rounded-[10px] shadow-[0px_4px_30px_0px_rgba(0,0,0,0.1)]">
+    <div className="w-55 h-50 border-1 border-gray-300 bg-white rounded-[10px] shadow-[0px_4px_30px_0px_rgba(0,0,0,0.1)]">
       <div className="flex justify-end">
         <img 
           src="/images/DeleteButton.svg" 
@@ -42,37 +59,25 @@ const NavProfileModal = ({onClose}: ProfileModalProps) => {
 
       <div className="h-px my-3 mx-4 bg-gray-400"/>
       
-      <div className="flex flex-col px-4 gap-1">
-        <button
-          onClick={() => handleAction("edit")}
-          className={`p-2 w-40 h-10 flex items-center gap-1 cursor-pointer hover:scale-105 transition-transform duration-300 text-body1
-            ${active === "edit"
-              ? "text-black bg-gray-200 rounded-[5px] font-medium"
-              : "text-gray-600"
-            }`}
-        >
-          <img 
-            src={active === "edit" ? "/images/ExitOnIcon.svg" : "/images/ExitOffIcon.svg"}
-            alt="프로필 수정 아이콘"
-            className="w-6 h-6"
-          />
-          프로필 수정하기
-        </button>
-        <button
-          onClick={() => handleAction("logout")}
-          className={`p-2 w-40 h-10 flex items-center gap-1 cursor-pointer hover:scale-105 transition-transform duration-300 text-body1
-            ${active === "logout"
-              ? "text-black bg-gray-200 rounded-[5px] font-medium"
-              : "text-gray-600"
-            }`}
-        >
-          <img 
-            src={active === "logout" ? "/images/ExitOnIcon.svg" : "/images/ExitOffIcon.svg"}
-            alt="로그아웃 아이콘"
-            className="w-6 h-6"
-          />
-          로그아웃
-        </button>
+      {/* 버튼 */}
+      <div className="flex flex-col px-4">
+        {actions.map((action) => (
+          <button
+            key={action.key}
+            onClick={action.onClick}
+            className={`p-2 w-45 h-10 flex items-center gap-1 cursor-pointer hover:scale-105 transition-transform duration-300 text-body1
+              ${active === action.key
+                ? "text-black bg-gray-200 rounded-[5px] font-medium"
+                : "text-gray-600"}`}
+          >
+            <img
+              src={active === action.key ? commonIcon.on : commonIcon.off}
+              alt={`${action.label} 아이콘`}
+              className="w-6 h-6"
+            />
+            {action.label}
+          </button>
+        ))}
       </div>
 
       {/* 모달 */}
