a7fc713cb78d26d25d8e9f06d2dcf8a33e0f23c7
seokhoon <excava.hoon76@gmail.com>
2025-07-16 11:27:46 +0900

    fix : viteconfigjs

--- FE_LXD/vite.config.ts
+++ FE_LXD/vite.config.ts
@@ -4,6 +4,23 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "./index.html",
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+  base: "./",
   plugins: [react(), tailwindcss()],
 });
