8b9427e54e85fafd9d521c8f5732cc3af8a2954c
minjuik <140732219+minjuik@users.noreply.github.com>
2025-07-25 14:44:20 +0900

    Design: LoginPage 디자인 수정

--- FE_LXD/src/layouts/HomeLayout.tsx
+++ FE_LXD/src/layouts/HomeLayout.tsx
@@ -2,7 +2,9 @@ import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
-    <div className="min-h-screen">
+    <div
+      className="min-h-screen bg-white"
+    >
       <Outlet />
     </div>
   );
