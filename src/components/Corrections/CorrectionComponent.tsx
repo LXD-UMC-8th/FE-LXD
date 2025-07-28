114857fb9d179bfdc208ffed2848e5a488eb3d1d
cchaeyoon <codbs0722@gmail.com>
2025-07-28 23:58:28 +0900

    [DESGIN]: CorrectionComponent CSs 수정

--- src/components/Corrections/CorrectionComponent.tsx
+++ src/components/Corrections/CorrectionComponent.tsx
@@ -9,39 +9,61 @@ const CorrectionComponent = () => {
   }
 
   return (
-    <div className="w-full max-w-[800px] h-80 bg-white rounded-[10px] border border-gray-300">
+    <div className="w-full max-w-[800px] h-[400px] bg-white rounded-[10px] border border-gray-300">
       <div className="flex flex-col">
         <div className="flex items-center justify-between px-6 pt-5">
           <ProfileComponent />
-          <img
-            src={liked ? "/images/HeartIcon.svg" : "/images/EmptyHeartIcon.svg"}
-            alt={liked ? "채워진 하트" : "빈 하트"}
-            className="w-6 h-6 cursor-pointer"
-            onClick={handleLikeToggle}
-          />
         </div>
 
         {/* 본문 */}
         <div className="flex flex-col gap-3 px-8 pt-5">
-          <div className="flex gap-2">
-            <div className="w-1 h-6 bg-[#4170FE]"/>
-            <p className="text-body1 font-semibold">오늘<span className="text-[#4170FE]">은</span> 피자데이입니다</p>
+          <div className="flex flex-col gap-2">
+            <p className="text-body1 font-semibold">오늘는 피자데이입니다</p>
+            <div className="flex gap-2">
+              <div className="w-1 h-6 bg-primary-500"/>
+              <p className="text-body1 font-semibold text-primary-500">오늘은 피자데이입니다</p>
+            </div>
+
           </div>
           <p className="text-body2">‘오늘’ 뒤의 보조사로는 ‘는’ 보다는 ‘은’이 더 적합합니다. 오늘에 종성이 있기 때문인데요. 앞말에 종성이 있다면 그 뒤에는 은이 와야합니다. 어쩌고 저쩌고 교정을 </p>
         </div>
+
+        {/* 메모 + 좋아요 */}
+        <div className="flex items-center text-body2 text-gray-700 gap-3 ml-auto mr-5">
+          {/* 메모 */}
+          <div className="flex gap-1">
+            <img />
+            <p>메모 수</p>
+          </div>
+          {/* 좋아요 */}
+          <div className="flex gap-1">
+            <img
+              src={liked ? "/images/HeartIcon.svg" : "/images/EmptyHeartIcon.svg"}
+              alt={liked ? "채워진 하트" : "빈 하트"}
+              className="w-5 h-5 cursor-pointer"
+              onClick={handleLikeToggle}
+            />
+            <p>좋아요 수</p>
+          </div>
+        </div>
       
         {/* 메모 */}
-        <div className="flex items-center bg-gray-200 rounded-[5px] border border-gray-300 mt-8 mx-7 px-3 py-2 gap-2">
-          <img
-            src="/images/MemoPlusIcon.svg"
-            alt="메모 추가"
-            className="w-4 h-4"
-          />
-          <input 
-            type="text"
-            placeholder="메모를 추가하기"
-            className="w-full h-full text-body1 text-gray-600 bg-transparent outline-none"
-          />
+        <div className="flex items-center gap-2 mt-7 mx-7">
+          <div className="flex-1 flex items-center bg-gray-100 rounded-[5px] border border-gray-300 px-2 py-2 gap-2">
+            <img
+              src="/images/MemoPlusIcon.svg"
+              alt="메모 추가"
+              className="w-4 h-4"
+            />
+            <input 
+              type="text"
+              placeholder="메모를 추가하기"
+              className="w-full h-full text-body1 text-gray-600 bg-transparent outline-none"
+            />
+          </div>
+          <button className="px-3 py-2 rounded-[5px] bg-primary-500 text-white whitespace-nowrap text-body1 cursor-pointer hover:scale-105 transition transform-300">
+              수정하기
+          </button>
         </div>
 
         <div className="border-t border-gray-300 mt-5"/>
