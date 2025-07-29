16a094edd8607e654a344b2fdfe52c46d975f124
minjuik <140732219+minjuik@users.noreply.github.com>
2025-07-29 16:21:25 +0900

    feat: 로그인 요청 API 연결

---
+++ FE_LXD/src/apis/auth.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  result: {
+    accessToken: string;
+    member: {
+      memberId: number;
+      email: string;
+      username: string;
+      nickname: string;
+      profileImg: string;
+      language: string;
+    };
+  };
+}
+
+// 로그인 요청 API
+export const postSignin = async (
+  payload: LoginRequest
+): Promise<LoginResponse> => {
+  const { data } = await axios.post<LoginResponse>("/auth/login", payload, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return data;
+};
