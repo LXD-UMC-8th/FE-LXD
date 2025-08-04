7edf6d7ec41941d75c90bf9dc26f6b1ff6db7ee9
cchaeyoon <codbs0722@gmail.com>
2025-08-04 23:29:31 +0900

    [API]: 교정 댓글 조회 API작성

--- src/apis/correctionComment.ts
+++ src/apis/correctionComment.ts
@@ -1,4 +1,4 @@
-import type { CorrectionCommentRequestDTO, CorrectionCommentResponseDTO } from "../utils/types/correctionComment";
+import type { CorrectionCommentGetRequestDTO, CorrectionCommentGetResponseDTO, CorrectionCommentRequestDTO, CorrectionCommentResponseDTO } from "../utils/types/correctionComment";
 import { axiosInstance } from "./axios";
 
 export const postCorrectionComments = async (
@@ -8,6 +8,23 @@ export const postCorrectionComments = async (
     const { data } = await axiosInstance.post<CorrectionCommentResponseDTO>(
         `/corrections/${correctionId}/comments`,
         body,
+    );
+    return data;
+}
+
+export const getCorrectionComments = async (
+    body: CorrectionCommentGetRequestDTO,
+): Promise<CorrectionCommentGetResponseDTO> => {
+    const { diaryId, page, size } = body;
+
+    const { data } = await axiosInstance.get<CorrectionCommentGetResponseDTO>(
+        `/diaries/${diaryId}/comments`,
+        {
+            params: {
+                page,
+                size,
+            },
+        },
     );
     return data;
 }
--- src/utils/types/correctionComment.ts
+++ src/utils/types/correctionComment.ts
@@ -3,16 +3,33 @@ import type { APIResponse } from "./APIresponse";
 // 교정 댓글 작성
 export type CorrectionCommentRequestDTO = {
     content: string;
-}
+};
 
 export type CorrectionCommentDTO = {
     commentId: number;
+    parentId: number | null;
     memberId: number;
     username: string;
     nickname: string;
     profileImage: string;
     content: string;
+    likeCount: number;
     createdAt: string;
-}
+    replyCount: number;
+    replies: CorrectionCommentDTO[];
+    liked: boolean;
+};
 
 export type CorrectionCommentResponseDTO = APIResponse<CorrectionCommentDTO>;
+
+// 일기 댓글 조회
+export type CorrectionCommentGetRequestDTO = {
+    diaryId: number;
+    page?: number;
+    size?: number;
+};
+
+export type CorrectionCommentGetResponseDTO = APIResponse<{
+    content: CorrectionCommentDTO[];
+    totalElements: number;
+}>;
