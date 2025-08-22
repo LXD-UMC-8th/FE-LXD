83fd8751b83fba244927f53041178987dff00f64
seokhoon <excava.hoon76@gmail.com>
2025-08-22 14:22:33 +0900

    fix : delete quotes in commoncomponent

--- src/hooks/useCleanHtmlRemoveImg.ts
+++ src/hooks/useCleanHtmlRemoveImg.ts
@@ -11,8 +11,12 @@ export function useCleanHtmlRemoveImg(htmlString: string | null): ReactNode {
     const sanitized = DOMPurify.sanitize(htmlString, {
       FORBID_TAGS: REMOVE_TAGS,
     });
-    const cleaned = sanitized.replace(/^"(.*)"$/, "$1");
+    const cleaned = sanitized.startsWith('"') ? sanitized.slice(1) : sanitized;
 
-    return parse(cleaned);
+    return parse(
+      cleaned.lastIndexOf('"') === cleaned.length - 1
+        ? cleaned.slice(0, -1)
+        : cleaned
+    );
   }, [htmlString]);
 }
